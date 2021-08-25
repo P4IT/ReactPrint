96f44eff19af9a3b857d91876937c0bdfb725327
S-Hoppe <s.hoppe@p4it.de>
2021-08-25 22:07:42 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ReactPrint',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
